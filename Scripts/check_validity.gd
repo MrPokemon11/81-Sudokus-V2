2c9f515d4e2ba61152ae5f6ea5fbe8be913e069d
WillowDWisp <devon.rogers1@ontariotechu.net>
2026-05-11 08:08:34 -0400

    work on inputs
    - only 1 character is allowed in a field
    - invalid characters are ignored
    - changing the color of the text doesn't work yet

--- Scripts/check_validity.gd
+++ Scripts/check_validity.gd
@@ -1,10 +1,11 @@
 extends Node
 
+var allowedInputs
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var allowedInputs = get_parent().get_meta("validInputs")
-	self.text_changed.connect(_on_text_changed.bind(self, allowedInputs))
+	allowedInputs = get_parent().get_meta("validInputs")
+	self.text_changed.connect(_on_text_changed.bind(self))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,8 +13,29 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_text_changed(textfield: TextEdit, allowedInputs: Array) -> void:
-	pass # Replace with function body.
+func _on_text_changed(textfield: TextEdit) -> void:
+	# ensure no more than 1 line
+	if (textfield.get_line_count() > 1):
+		textfield.remove_line_at(1)
+
+	# ensure no more than 1 character, setting it to the most recent (valid) input
+	if (textfield.text.length() > 1):
+		var isValid = false
+		for valid_inputs in allowedInputs:
+			if (textfield.text.right(1) == str(valid_inputs)):
+				isValid = true
+				textfield.text = textfield.text.right(1)
+				textfield.set_caret_column(1)
+				break
+		if (!isValid):
+			textfield.text = textfield.text.left(1)
+			textfield.set_caret_column(1)
+		
+
+	# check the row for errors
+	for fields in textfield.get_meta("Row"):
+		if(get_node(fields) != textfield):
+			_check_for_errors(get_node(fields), textfield.text)
 
 
 # the above class needs to do the following:
@@ -23,6 +45,12 @@ func _on_text_changed(textfield: TextEdit, allowedInputs: Array) -> void:
 #	- this uses the metadata of the TextEdit fields
 #	- if there are errors, they should be highlighted in red
 # - ensure that (for normal puzzles) only the digits 1 to 9 are present
+
+func _check_for_errors(textfield: TextEdit, compare_value: Variant) -> void:
+	if(str(compare_value) == textfield.text):
+		_handle_correctness_changed(textfield, true)
+	else:
+		_handle_correctness_changed(textfield, false)
 
 func _handle_correctness_changed(textfield: TextEdit, isIncorrect: bool) -> void:
 	if(isIncorrect):
