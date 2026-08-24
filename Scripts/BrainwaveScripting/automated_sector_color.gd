a9ac509135bbfcd077976d59c491da143bd4dbd7
WillowDWisp <devon.rogers1@ontariotechu.net>
2026-08-24 11:54:47 -0400

    Update automated_sector_color.gd

--- Scripts/BrainwaveScripting/automated_sector_color.gd
+++ Scripts/BrainwaveScripting/automated_sector_color.gd
@@ -1,7 +1,12 @@
 @tool
 extends EditorScript
 
+var numSectors = 7
 
 func _run() -> void:
-	
+	for sector in numSectors:
+		var currSector = sector+1
+		var currSectorGroup = "Sector" + currSector.to_string()
+		for node in get_scene().get_tree().get_nodes_in_group(currSectorGroup):
+			node.control.set_theme_item
 	pass # Replace with function body.
