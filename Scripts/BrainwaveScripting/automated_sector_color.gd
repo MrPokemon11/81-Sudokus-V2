d21fd03835ddcbf9e0337f3bfa30409c5e9035cd
WillowDWisp <devon.rogers1@ontariotechu.net>
2026-08-24 20:46:05 -0400

    work on legally distinct 2

--- Scripts/BrainwaveScripting/automated_sector_color.gd
+++ Scripts/BrainwaveScripting/automated_sector_color.gd
@@ -5,8 +5,8 @@ var numSectors = 7
 
 func _run() -> void:
 	for sector in numSectors:
-		var currSector = sector+1
-		var currSectorGroup = "Sector" + currSector.to_string()
+		var currSector : int = sector+1
+		var currSectorGroup = "Sector" + str(currSector)
 		for node in get_scene().get_tree().get_nodes_in_group(currSectorGroup):
-			node.control.set_theme_item
+			node.theme = load("res://Themes/Sectors/" + currSectorGroup +".tres")
 	pass # Replace with function body.
